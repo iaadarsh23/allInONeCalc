c67238624b77b7bef0224ded13780d362ce0206a
Adarsh <adarshtrip2306@gmail.com>
2024-11-16 21:59:30 +0530

    push

---
+++ public/app.js
@@ -0,0 +1,5 @@
+const userInput= document.getElementById('input');
+const userOutput= document.getElementById('output');
+const clearbtn= document.getElementById('clear');
+
+const btns= document.querySelectorAll('buttons')
