0275d6b0ad34b4fe11cd7b5f2f10e8f9e2f632ab
Adarsh <adarshtrip2306@gmail.com>
2024-11-16 16:40:45 +0530

    again installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./*.{html,js}"
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
