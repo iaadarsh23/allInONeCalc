bc3dbd0970de930c0d7c1bca972c3bb83e9ec057
Adarsh <adarshtrip2306@gmail.com>
2024-11-16 22:56:51 +0530

    push

--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
 const userInput= document.getElementById('input');
 const userOutput= document.getElementById('output');
 const clearbtn= document.getElementById('clear');
+//selecting all the input btns at all
+const btns= document.querySelectorAll('input[type="button"]');
 
-const btns= document.querySelectorAll('buttons')
+function calc(){
+    //looping through each btns
+    btns.forEach(button =>{
+        button.addEventListener('click',()=>{
+            if(button.dataset.type==='number'){
+                // input element me .value use krte hai data update krne ke liye
+                userInput.value= button.value;
+            }
+            else if(button.dataset.type==='operator'){
+                userInput.value= button.value;
+            }
+
+        })
+    })
+}
+calc();
