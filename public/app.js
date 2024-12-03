3feae15e973255d327ae32e0c67af53b7b8022bd
Adarsh <adarshtrip2306@gmail.com>
2024-12-03 23:33:47 +0530

    main html done

--- public/app.js
+++ public/app.js
@@ -1,7 +1,4 @@
-
-//let api="569e019ea0880c8c2b20220ac28d1fe7";
-
-
+// let api = "569e019ea0880c8c2b20220ac28d1fe7";
 
 // async function get() {
 //     try {
@@ -15,8 +12,7 @@
 //         let data = await response.json();
 //         console.log(data);
 //         let city= data.list[0].main.aqi;
-        
-       
+
 //         // Display data
 //         const result = document.getElementById("js-result");
 //         result.innerHTML = ` <p> here is the forcst</p>
@@ -34,10 +30,9 @@
 //             o3: ${data.list[0].components.o3}
 //             </li>
 
-         
 //          </ul>
 //             `
-            
+
 //     } catch (error) {
 //         console.error(`Error fetching data: ${error.message}`);
 //     }
@@ -45,73 +40,60 @@
 
 //
 
-    function showLoc(position){
-         let lat=position.coords.latitude;
-         let lon=position.coords.longitude;
+function showLoc(position) {
+	let lat = position.coords.latitude;
+	let lon = position.coords.longitude;
 
-         async function Loc() {
-            let response= await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`)
-            try {
-                if(!response.ok){
-                    throw new Error("Network issue", response.status)
-                }
-                let data= await response.json();
-                show(data);
-
-                
-            } catch (error) {
-                console.log("error", error.message);
-                
-            }
-            ;
-            
-            
-         }
-         Loc();
-
-
-        
-    
-    }
-
-
-async function wether() {
-    const city=document.getElementById("srch").value.trim();
-    
-   
-    try {
-        // Fetch weather data from the API
-        const strt= performance.now();
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`);
-        
-        // Check if the response is not ok
-        if (!response.ok) {
-            throw new Error(`Network issue! Status: ${response.status}`);
-        }
-        const finish= performance.now();
-        // Parse the JSON response
-        let data1 = await response.json();
-        
-        console.log(finish-strt);
-        console.log(data1);
-
-        // Call a function to display the weather data
-        show(data1);
-
-    } catch (error) {
-        // Catch and log errors
-        console.error(`Cannot access weather data: ${error.message}`);
-    }
+	async function Loc() {
+		let response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`
+		);
+		try {
+			if (!response.ok) {
+				throw new Error("Network issue", response.status);
+			}
+			let data = await response.json();
+			show(data);
+		} catch (error) {
+			console.log("error", error.message);
+		}
+	}
+	Loc();
 }
 
+async function wether() {
+	const city = document.getElementById("srch").value.trim();
 
+	try {
+		// Fetch weather data from the API
+		const strt = performance.now();
+		let response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`
+		);
 
-function show(vle){
-   
-    const result = document.getElementById("js-result");
-    
-    
-    result.innerHTML=`
+		// Check if the response is not ok
+		if (!response.ok) {
+			throw new Error(`Network issue! Status: ${response.status}`);
+		}
+		const finish = performance.now();
+		// Parse the JSON response
+		let data1 = await response.json();
+
+		console.log(finish - strt);
+		console.log(data1);
+
+		// Call a function to display the weather data
+		show(data1);
+	} catch (error) {
+		// Catch and log errors
+		console.error(`Cannot access weather data: ${error.message}`);
+	}
+}
+
+function show(vle) {
+	const result = document.getElementById("js-result");
+
+	result.innerHTML = `
 
         <p> wethere data</p>
         <ul>
@@ -123,27 +105,19 @@ function show(vle){
         <li></li>
         </ul>
     
-    `
-    newPara='';
+    `;
+	newPara = "";
 }
 
-document.getElementById("btn").addEventListener('click', ()=>{
-   
-    wether();
-    
+document.getElementById("btn").addEventListener("click", () => {
+	wether();
+});
 
-})
-
-function myloc(){
-    const result = document.getElementById("js-result");
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showLoc);
-    }
-    else{
-        result.innerHTML="cnnt access";
-    }
-    
+function myloc() {
+	const result = document.getElementById("js-result");
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showLoc);
+	} else {
+		result.innerHTML = "cnnt access";
+	}
 }
-
-
-
